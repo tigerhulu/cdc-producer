e88c67c03f4a0aa48e62ffc63a760cbd3a87eb14
xingnan.hxn <xingnan.hxn@alibaba-inc.com>
2023-08-10 21:31:43 +0800

    INIT

---
+++ src/main/java/com/tigerhulu/demo/cdcproducer/CdcProducerApplication.java
@@ -0,0 +1,13 @@
+package com.tigerhulu.demo.cdcproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdcProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdcProducerApplication.class, args);
+	}
+
+}
